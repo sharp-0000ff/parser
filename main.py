b4ab8a61cd6df774a1d2b16be3d4b300ecfc39c1
Andrei Bartosh <sharp.0000ff@gmail.com>
2020-10-14 21:44:22 +0300

    getting all contacts from the site

--- main.py
+++ main.py
@@ -6,5 +6,7 @@ import requests
 
 
 link = 'https://www.mebelshara.ru/contacts'
-response = requests.get(link)
-print(response.text)
+response = requests.get(link).text
+soup = BeautifulSoup(response, 'html.parser')
+items = soup.find_all('div', {'class': 'city-item'})
+print(items)
