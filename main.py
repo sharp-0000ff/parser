5f89e509f356ea8c17c77ea128d9044c9ea73a09
Andrei Bartosh <sharp.0000ff@gmail.com>
2020-10-14 23:20:03 +0300

    writing a function to get store data

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ response = requests.get(link).text
 soup = BeautifulSoup(response, 'html.parser')
 items = soup.find_all('div', {'class': 'city-item'})
 
+output_list = list()
 city = str()
 address = str()
 latitude = str()
@@ -17,22 +18,27 @@ store_name = str()
 phone = str()
 opening_hours = str()
 work_days = str()
-output_list = list()
+
+
+def get_value(first_separator, second_separator):
+    return re.split(first_separator, str(item))[1].split(second_separator)[0]
+
 
 for item in items:
-    city = re.split('name">', str(item))[1].split('</h4>')[0]
-    address = re.split('address="', str(item))[1].split('"')[0]
-    latitude = re.split('latitude="', str(item))[1].split('"')[0]
-    longitude = re.split('longitude="', str(item))[1].split('"')[0]
-    store_name = re.split('shop-name">', str(item))[1].split('</')[0]
-    opening_hours = re.split('mode2="', str(item))[1].split('"')[0]
-    work_days = re.split('mode1="', str(item))[1].split('"')[0]
-    phone = re.split('phone">', str(item))[1].split('</')[0]
+    city = get_value('name">', '</h4>')
+    address = get_value('address="', '"')
+    latitude = get_value('latitude="', '"')
+    longitude = get_value('longitude="', '"')
+    store_name = get_value('shop-name">', '</')
+    opening_hours = get_value('mode2="', '"')
+    work_days = get_value('mode1="', '"')
+    phone = get_value('phone">', '</')
     output_list.append({
         'address': f'{city}, {address}',
         'latlon': [latitude, longitude],
         'name': store_name,
         'phones': [phone],
         'working_hours': [work_days, opening_hours]})
+
 for line in output_list:
     print(line)
